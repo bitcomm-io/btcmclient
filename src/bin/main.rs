9e7087fae119ed5e51cac075929e534048b1a03a
Bitcomm <jeff@bitcomm.io>
2024-02-12 20:32:30 +0800

    Update main.rs

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -9,24 +9,21 @@ use btcmtools::command::{self, TextToUser, UserPass};
 use bytes::Bytes;
 // use bytes::Bytes;
 // use btcmbase::datagram::MessageDataGram;
-use s2n_quic::{client::Connect, stream::SendStream, Client};
-use std::{error::Error, net::SocketAddr};
-use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
+use s2n_quic::{client::Connect, stream::{ReceiveStream, SendStream}, Client, Connection};
+use std::{error::Error, net::SocketAddr, sync::Arc, time::Duration};
+use tokio::{io::{AsyncBufReadExt, AsyncWriteExt, BufReader}, sync::Mutex};
 
 /// 注意: 该证书仅供演示目的使用！
 // 静态字符串，包含 PEM 格式的证书内容
 pub static CERT_PEM: &str = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/../certs/cert.pem"));
 static DEVICE_ID: u32 = 0x48dc7346;
-// 异步主函数，返回 Result 类型，其中错误类型为实现 Error trait 的任意类型的 trait 对象
-#[tokio::main]
-// #[repr(align)]
-async fn main() -> Result<(), Box<dyn Error>> {
-    // 创建 Client 实例，并通过 builder 模式配置 TLS 和 I/O
+
+
+async fn get_quic_connect() -> Result<Option<Connection>,Box<dyn Error>> {
     let client = Client::builder()
         .with_tls(CERT_PEM)? // 使用指定的 PEM 证书配置 TLS
         .with_io("0.0.0.0:0")? // 指定 I/O 地址
         .start()?; // 启动 Client
-
     // 定义 Socket 地址，解析字符串并返回 SocketAddr 类型
     // let addr: SocketAddr = "192.168.3.6:4433".parse()?;
     let addr: SocketAddr = "127.0.0.1:9563".parse()?;
@@ -35,28 +32,21 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let connect = Connect::new(addr).with_server_name("localhost");
     // 连接服务器，获取 Connection 实例
     let mut connection = client.connect(connect).await?;
-
     // 确保连接不会因不活动而超时
     connection.keep_alive(true)?;
 
-    // 打开一个新的双向流，并将其拆分为接收和发送两个部分
-    let stream = connection.open_bidirectional_stream().await?;
-    let (mut receive_stream, mut send_stream) = stream.split();
+    Result::Ok(Some(connection))
+}
+async fn rece_data(stream:Arc<Mutex<ReceiveStream>>) {
+    let mut receive_stream = stream.lock().await;
+    while let Ok(Some(data)) = receive_stream.receive().await {
+        eprintln!("read from server {:?}", data);
+    }
+}
 
-    // 创建异步任务，将服务器的响应复制到标准输出
-    tokio::spawn(async move {
-        while let Ok(Some(data)) = receive_stream.receive().await {
-            eprintln!("Stream opened data    from {:?}", data);
-        }
-        // let mut stdout = tokio::io::stdout();
-        // let _ = tokio::io::copy(&mut receive_stream, &mut stdout).await;
-    });
+async fn inputext(stream:Arc<Mutex<SendStream>>) -> Result<Option<()>,Box<dyn Error>> {
 
-    // 新建一个CommandDataGram
-    // 复制标准输入的数据并发送到服务器
-    // let mut stdin = tokio::io::stdin();
-    // tokio::io::copy(&mut stdin, &mut send_stream).await?;
-
+    let mut send_stream = stream.lock().await;
     let stdin = tokio::io::stdin();
     let mut reader = BufReader::new(stdin);
 
@@ -104,10 +94,42 @@ async fn main() -> Result<(), Box<dyn Error>> {
         } else {
         send_stream
             .send(Bytes::from(input.clone()))
-            .await
-            .expect("error!");
+            .await?;
         }
         input.clear();
+    }    
+    Ok(Option::None)
+}
+static _TIME_OUT_:Duration = Duration::from_secs(5);
+// 异步主函数，返回 Result 类型，其中错误类型为实现 Error trait 的任意类型的 trait 对象
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    //
+    while 1==1 {
+        // 
+        let mut connection = get_quic_connect().await.unwrap().unwrap();//client.connect(connect).await?;
+
+        // 打开一个新的双向流，并将其拆分为接收和发送两个部分
+        let stream = connection.open_bidirectional_stream().await?;
+        //
+        let (receive_stream, send_stream) = stream.split();
+        //
+        let rs = Arc::new(Mutex::new(receive_stream));
+        //
+        tokio::spawn(async move {rece_data(rs).await;});
+        //
+        let ss = Arc::new(Mutex::new(send_stream));
+        // 
+        let res = inputext(ss).await;
+        match res {
+            Ok(_) => {
+
+            }
+            Err(err) => {
+                eprintln!("quic error =  {:?}", err);
+            }
+        }
+        eprintln!("client re connection! ");
     }
     Ok(()) // 成功返回 Ok(())
 }
